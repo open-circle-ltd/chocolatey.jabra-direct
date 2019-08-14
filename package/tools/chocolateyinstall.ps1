1b86b1906d8a548213e893c690b9d3187342ad81
Simon Baerlocher <s.baerlocher@sbaerlocher.ch>
2019-08-14 21:03:03 +0200

    fix milk

--- package/tools/chocolateyinstall.ps1
+++ package/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $ErrorActionPreference = 'Stop';
 
 $toolsDir            = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $PackageParameters   = Get-PackageParameters
-$urlPackage = "https://jabraxpressonlineprdstor.blob.core.windows.net/jdo/JabraDirectSetup.exe"
-$checksumPackage = "3c3b2e2f3cd524d04074992724ef105abf68fbbbfc92b28c60ec5f8582388742d72ddc0d7aac1c150adc2ba7bb3e17e0bd3e843aad5e8138a676440876713ca1"
+$urlPackage = 'https://jabraxpressonlineprdstor.blob.core.windows.net/jdo/JabraDirectSetup.exe'
+$checksumPackage = '3c3b2e2f3cd524d04074992724ef105abf68fbbbfc92b28c60ec5f8582388742d72ddc0d7aac1c150adc2ba7bb3e17e0bd3e843aad5e8138a676440876713ca1'
 $checksumTypePackage = 'SHA512'
 
 Import-Module -Name "$($toolsDir)\helpers.ps1"
