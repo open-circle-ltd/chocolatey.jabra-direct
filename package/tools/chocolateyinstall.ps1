959693402db4e455bd9bd26bdf7f5bbcad1d2980
Benaiah Catheraso <benaiah@itigo.ch>
2021-01-15 09:00:22 +0100

    Update 5.0.17635

--- package/tools/chocolateyinstall.ps1
+++ package/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $ErrorActionPreference = 'Stop';
 $toolsDir            = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $PackageParameters   = Get-PackageParameters
 $urlPackage = 'https://jabraxpressonlineprdstor.blob.core.windows.net/jdo/JabraDirectSetup.exe'
-$checksumPackage = '6cef272d861d5525f72bd15546120f54a49932b7ccf63aa3348ec506248963a769f097bba277c3079d004a4ad1a6d1be6dd1db94a35cb5a2332254f24055415a'
+$checksumPackage = '5A197D742CF8454CDF7095783653D61FA44A106700C12E7195D08BBD0D02F2183D8C2977455BEE8D2384EDF3E8BA52DB512A40A2B1BCD7A770376236934057C6'
 $checksumTypePackage = 'SHA512'
 
 Import-Module -Name "$($toolsDir)\helpers.ps1"
